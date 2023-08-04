09165b1df03ef31f020ddd160ac8de04d384a35d
Luanzim1327 <luanmas27@gmail.com>
2023-08-04 20:06:35 -0300

    ig_news_rockeseat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { SubscribeButton } from '@/components/SubscribeButton'
+import styles from './home.module.scss'
+import { Metadata } from "next"
+import { stripe } from '@/services/stripe'
 
-export default function Home() {
+
+export const metadata: Metadata = {
+  title: 'ig.news | Home',
+}
+
+export default async function Home() {
+
+  const price = await stripe.prices.retrieve('price_1NbT0yAGY9OYkasHGDayCxR0' , {
+    expand:['product']
+  })
+
+  const product = {
+    priceId : price.id,
+    amount : new Intl.NumberFormat('en-US' , {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount! / 100),
+  };
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+          <span>👏 Hey , welcome</span>
+          <h1>News about the <span>React</span> world</h1>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Get access to all the publications <br/>
+            <span>for {product.amount} month</span>
           </p>
-        </a>
-      </div>
+          <SubscribeButton priceId={product.priceId}/>
+      </section>
+      <img src="/images/avatar.svg" alt="girl coding" />
     </main>
   )
 }
